0a5278a77b5a4369762b2b0a27fa491caa232906
Josiah Campbell <josiahcampbell64@gmail.com>
2016-10-31 22:06:18 -0400

    Initial re-commit after copy

---
+++ app/src/main/java/com/tutorial/app/Main.java
@@ -0,0 +1,9 @@
+package com.tutorial.app;
+
+import com.tutorial.api.HelloAPI;
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println(HelloAPI.invokeHello("world"));
+	}
+}
